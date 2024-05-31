d3a03eea150c659c553d86f05316d5643e8e75ce
Krunal N. Khobragade <krunalkk123@gmail.com>
2024-05-31 16:51:03 +0530

    Basic strucure for SMS

---
+++ app/src/main/java/storehub/api/app/Main.java
@@ -0,0 +1,12 @@
+package storehub.api.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+
+}
